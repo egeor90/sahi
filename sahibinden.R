aa9d0f0b1b827a27c37c3d28eb03f00e23dcf92b
Ege Or <35030660+egeor90@users.noreply.github.com>
2020-11-01 01:22:50 +0300

    Updated for one page scraping

--- sahibinden.R
+++ sahibinden.R
@@ -58,54 +58,57 @@ df$caption <- as.character(df$caption)
 
 df_ <- df
 
-
-# late pages --------------------------------------------------------------
-
-max_page <- ifelse(max_page >= 20, 20, max_page) # It can fetch maximum 1000 results.
-
-for(i in 2:max_page){
-  url[i] <- paste0("https://www.sahibinden.com/",tolower(brand),"-",tolower(model),"?viewType=Classic&pagingOffset=",50*(i-1),"&pagingSize=50")
-}
-
-url <- url[-1]
-
-for(i in 1:length(url)){
-  html <- read_html(url[i])
+if(max_page <= 1 || length(max_page)==0){
+  dt <- dplyr::data_frame(df_)
+  dt <- dt[!duplicated(dt),]
+}else if(max_page > 1){
   
-  max_page <- get_max_page(html = html)
+  max_page <- ifelse(max_page >= 20, 20, max_page)
   
-  captions <- get_captions(html)
-  links_ <- get_links(html)
-  get_yrkmcol(html)
-  get_price(html)
-  date_ <- get_date(html)
-  get_location(html=html)
-  
-  df <- data.frame(caption = captions, 
-                   link = links_, 
-                   year = year_, 
-                   km = km_, 
-                   color = color_, 
-                   price = price_, 
-                   price_unit = price_unit, 
-                   date = date_,
-                   city = location_city,
-                   district = location_district)
-  
-  df$km <- as.numeric(gsub("\\.","",df$km))
-  df$link <- as.character(df$link)
-  df$caption <- as.character(df$caption)
-  
-  if(!exists("df_")){
-    df_ <- df
-  }else{
-    df_ <- rbind(df_, df)
+  for(i in 2:max_page){
+    url[i] <- paste0("https://www.sahibinden.com/",tolower(brand),"-",tolower(model),"?viewType=Classic&pagingOffset=",50*(i-1),"&pagingSize=50")
   }
+  
+  url <- url[-1]
+  
+  for(i in 1:length(url)){
+    html <- read_html(url[i])
+    
+    max_page <- get_max_page(html = html)
+    
+    captions <- get_captions(html)
+    links_ <- get_links(html)
+    get_yrkmcol(html)
+    get_price(html)
+    date_ <- get_date(html)
+    get_location(html=html)
+    
+    df <- data.frame(caption = captions, 
+                     link = links_, 
+                     year = year_, 
+                     km = km_, 
+                     color = color_, 
+                     price = price_, 
+                     price_unit = price_unit, 
+                     date = date_,
+                     city = location_city,
+                     district = location_district)
+    
+    df$km <- as.numeric(gsub("\\.","",df$km))
+    df$link <- as.character(df$link)
+    df$caption <- as.character(df$caption)
+    
+    if(!exists("df_")){
+      df_ <- df
+    }else{
+      df_ <- rbind(df_, df)
+    }
+  }
+  
+  
+  dt <- dplyr::data_frame(df_)
+  dt <- dt[!duplicated(dt),]
 }
-
-
-dt <- dplyr::data_frame(df_)
-dt <- dt[!duplicated(dt),]
 
 
 dt_out <- as.data.frame(dt)
