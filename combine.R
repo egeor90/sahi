2525832de224d01ad170e066926fa2764ac94342
Ege Or <35030660+egeor90@users.noreply.github.com>
2020-11-01 03:07:33 +0300

    Update combine.R

--- combine.R
+++ combine.R
@@ -23,3 +23,16 @@ for(i in 1:length(all_cars)){
 
 df <- do.call(rbind.data.frame, all_cars)
 rownames(df) <- as.numeric(1:nrow(df))
+
+df$caption <- as.character(df$caption)
+df$link <- as.character(df$link)
+df$year <- as.factor(df$year)
+df$km <- as.numeric(df$km)
+df$color <- as.factor(df$color)
+df$price <- as.numeric(df$price)
+df$price_unit <- as.factor(df$price_unit)
+df$date <- as.Date(df$date)
+df$city <- as.factor(df$city)
+df$district <- as.factor(df$district)
+df$brand <- as.factor(df$brand)
+df$model <- as.factor(df$model)
