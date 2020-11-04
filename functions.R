433283f5c72fb5f4a878d7580f580385cbb3937a
Ege Or <35030660+egeor90@users.noreply.github.com>
2020-11-04 20:10:52 +0300

    Update functions.R

--- functions.R
+++ functions.R
@@ -109,6 +109,45 @@ get_location <- function(html){
   assign("location_district",location_district, envir = .GlobalEnv)
 }
 
+combine_all <- function(){
+  suppressWarnings(suppressMessages(require(here)))
+  setwd(paste0(here(),"/data"))
+  
+  all_cars <- lapply(list.files(pattern="*.csv"), 
+                     function(x) read.csv(x,
+                                          fill=TRUE, 
+                                          header=TRUE, 
+                                          quote="", 
+                                          sep=";", 
+                                          stringsAsFactors = FALSE, 
+                                          encoding="UTF-8"))
+  
+  
+  for(i in 1:length(all_cars)){
+    names(all_cars)[i] <- tolower(all_cars[[i]][,12][1])
+  }
+  
+  
+  df <- do.call(rbind.data.frame, all_cars)
+  rownames(df) <- as.numeric(1:nrow(df))
+  
+  df$caption <- as.character(df$caption)
+  df$link <- as.character(df$link)
+  df$year <- as.factor(df$year)
+  df$km <- as.numeric(df$km)
+  df$color <- as.factor(df$color)
+  df$price <- as.numeric(df$price)
+  df$price_unit <- as.factor(df$price_unit)
+  df$date <- as.Date(df$date)
+  df$city <- as.factor(df$city)
+  df$district <- as.factor(df$district)
+  df$brand <- as.factor(df$brand)
+  df$model <- as.factor(df$model)
+  
+  df <- na.omit(df)
+  return(df)
+}
+
 
 car_train <- function(portion = 0.7, nround = 20000, output_file = "xgb.model"){
 
