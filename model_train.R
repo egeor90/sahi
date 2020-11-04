1f5c05c54c47c3cbfdd4adc50b4fc6abd2049884
Ege Or <35030660+egeor90@users.noreply.github.com>
2020-11-04 19:20:53 +0300

    Update model_train.R

--- model_train.R
+++ model_train.R
@@ -6,14 +6,16 @@ setwd(here())
 source("functions.R")
 
 # data preprocess
-ege <- data.table(combine_all())
+dt <- data.table(combine_all())
 
-ege <- ege[,-c(1,2,7,10)]
-ege <- ege[which(ege$km >= 5000),]
-ege <- ege[which(as.numeric(as.character(ege$year)) >= 2000),]
+dt <- dt[,-c(1,2,7,10)]
+dt <- dt[which(dt$km >= 5000),] # More than 5000 km.
+dt <- dt[which(as.numeric(as.character(dt$year)) >= 2000),] # Newer than 2000.
 
-ege$old <- as.numeric(Sys.Date()-ege$date)
-ege <- ege[,-c("date")]
-ege$year <- as.numeric(substr(Sys.Date(),1,4)) - as.numeric(as.character(ege$year))
+dt$old <- as.numeric(Sys.Date()-dt$date)
+dt <- dt[,-c("date")]
+dt$year <- as.numeric(substr(Sys.Date(),1,4)) - as.numeric(as.character(dt$year))
 
+
+# Train xGBoost model on dataset
 car_train(portion = 0.7, nround = 20000)
