e02c9f3584b92ab71fb3530fefd53526e9ab3cc7
Ege Or <35030660+egeor90@users.noreply.github.com>
2020-11-08 01:45:55 +0300

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-
 FILE="#!$(which Rscript)"
 
-echo "$FILE" | cat - functions.R > temp && mv temp functions.R
-echo "$FILE" | cat - predict.R > temp && mv temp predict.R
-echo "$FILE" | cat - model_train.R > temp && mv temp model_train.R
-echo "$FILE" | cat - sahibinden.R > temp && mv temp sahibinden.R
+if [[ "$(cat functions.R | head -n1)" != "$FILE" ]]; then
+    echo "$FILE" | cat - functions.R > temp && mv temp functions.R
+fi
+
+if [[ "$(cat predict.R | head -n1)" != "$FILE" ]]; then
+    echo "$FILE" | cat - predict.R > temp && mv temp predict.R
+fi
+
+if [[ "$(cat sahibinden.R | head -n1)" != "$FILE" ]]; then
+    echo "$FILE" | cat - sahibinden.R > temp && mv temp sahibinden.R
+fi
+
+if [[ "$(cat model_train.R | head -n1)" != "$FILE" ]]; then
+    echo "$FILE" | cat - model_train.R > temp && mv temp model_train.R
+fi
+
+sudo chmod +x *.R
